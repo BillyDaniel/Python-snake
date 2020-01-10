66dcdeb888ebc86f874f7e2c28d8334c57efb6aa
BillyDaniel <ddeleon@csumb.edu>
2020-01-09 17:08:57 -0800

    Working base needs work

--- snake.py
+++ snake.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 WIDTH, HEIGHT = 500, 500
 ROWS = 20
@@ -7,27 +8,30 @@ SPACE=WIDTH//ROWS
 class Snake:
     def __init__(self,surface):
         self.surface = surface
-        self.pos = []
         self.dirx = -SPACE
         self.diry = 0
         self.color = (255,0,0)
         self.body = []
-        self.length = 4
-        for x in range(self.length):
-            self.pos.append((10-x,10))
-            self.body.append(pygame.draw.rect(self.surface, self.color, (self.pos[x][0]*SPACE, self.pos[x][1]*SPACE, SPACE+1,SPACE+1)))
-        self.rect = self.body[0]
+        self.length = 2
+        for x in range(self.length-1):
+            self.body.append(pygame.draw.rect(self.surface, self.color, ((10+x+1)*SPACE, 10*SPACE, SPACE+1,SPACE+1)))
+        self.rect = pygame.draw.rect(self.surface, self.color, (10*SPACE, 10*SPACE, SPACE+1,SPACE+1))
+        self.fruit = self.make_apple()
 
     def move(self):
-        if self.boundary_check()  == True:
+        if self.boundary_check() or self.hit_check() == True:
             self.reset()
         else:
-            print(self.body)
-            self.body.insert(0,self.rect.move(self.dirx,self.diry))
-            self.body.pop()
-            self.rect = self.body[0]
+            self.body.insert(0,self.rect)
+            self.rect = self.rect.move(self.dirx,self.diry)
+            if self.rect == self.fruit:
+                self.fruit = self.make_apple()
+            else:
+                self.body.pop()
             for snake in self.body:
                 pygame.draw.rect(self.surface, self.color,snake)
+            pygame.draw.rect(self.surface, self.color,self.rect)
+            pygame.draw.rect(self.surface,(0,255,0),self.fruit)
 
     def direction(self):
         for event in pygame.event.get():
@@ -37,18 +41,22 @@ class Snake:
                 if event.key == pygame.K_UP and self.diry != SPACE:
                     self.dirx = 0
                     self.diry = -SPACE
+                    break
 
-                if event.key == pygame.K_DOWN and self.diry != -SPACE:
+                elif event.key == pygame.K_DOWN and self.diry != -SPACE:
                     self.dirx = 0
                     self.diry = SPACE
+                    break
 
-                if event.key == pygame.K_LEFT and self.dirx != SPACE:
+                elif event.key == pygame.K_LEFT and self.dirx != SPACE:
                     self.dirx = -SPACE
                     self.diry = 0
+                    break
 
-                if event.key == pygame.K_RIGHT and self.dirx != -SPACE:
+                elif event.key == pygame.K_RIGHT and self.dirx != -SPACE:
                     self.dirx = SPACE
                     self.diry = 0
+                    break
 
     def boundary_check(self):
         if any([
@@ -66,11 +74,20 @@ class Snake:
 
     def reset(self):
         self.body.clear()
-        for x in range(self.length):
-            self.body.append(pygame.draw.rect(self.surface, self.color, (self.pos[x][0]*SPACE, self.pos[x][1]*SPACE, SPACE+1,SPACE+1)))
-        self.rect = self.body[0]
+        for x in range(self.length-1):
+            self.body.append(pygame.draw.rect(self.surface, self.color, ((10+x+1)*SPACE, 10*SPACE, SPACE+1,SPACE+1)))
+        #self.rect = self.body[0]
+        self.rect = pygame.draw.rect(self.surface, self.color, (10*SPACE, 10*SPACE, SPACE+1,SPACE+1))
         self.dirx = -SPACE
         self.diry = 0
+
+    def make_apple(self):
+        fruit_x = rand_x = random.randrange(0, WIDTH, SPACE)
+        fruit_y = rand_y = random.randrange(0, HEIGHT, SPACE)
+        print(fruit_x,fruit_y)
+        return pygame.draw.rect(self.surface, (0,255,0), (fruit_x,fruit_y,SPACE+1,SPACE+1))
+
+
 
 class Map:
     def __init__(self,surface):
@@ -90,6 +107,9 @@ class Map:
         snake.direction()
         pygame.display.update()
         pygame.time.delay(200)
+
+
+
 
 
 def main():
